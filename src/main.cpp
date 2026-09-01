a36748a20a302c7f2114373753a0754198db8bd0
Jonathan Immanuel <jonathan.immanuel101@gmail.com>
2026-09-01 16:04:29 +1000

    Set up the C++ project and test program

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Kosaraju SCC project\n";
+  return 0;
+}
+
---
+++ tests/scc_tests.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Project test program is ready\n";
+  return 0;
+}
